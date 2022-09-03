006a67d578f746d16b0634b5b00775dc58171d6d
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-09-03 11:41:54 +0900

    Update main.js

--- BounceBall/main.js
+++ BounceBall/main.js
@@ -1,15 +1,20 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d")
-canvas.width=800;
+canvas.width=1000;
 canvas.height=800;
+
+balls=[];
+ballNumber = 100;
 
 class ball{
      constructor(x,y,rad,speed){
         this.x = x;
         this.y = y;
         this.rad = rad;
-        this.vx=speed;
-        this.vy=speed;
+        this.angle = (Math.random()*(Math.PI*2));
+        this.vx=Math.cos(this.angle)*speed;
+        this.vy=Math.sin(this.angle)*speed;
+        this.color="#" + Math.floor(Math.random() * 16777215).toString(16);
     }
     draw(){
         const minX = this.rad;
@@ -17,6 +22,7 @@ class ball{
         const minY = this.rad;
         const maxY = canvas.height - this.rad;
         ctx.beginPath();
+        ctx.fillStyle=this.color;
         ctx.arc(this.x,this.y,this.rad,Math.PI*2,false);
         if(this.x<=minX || this.x>=maxX){
             this.vx *= -1;
@@ -32,11 +38,19 @@ class ball{
     }
 }
 
-const box = new ball(400,400,20,5);
+function init(){
+    for(i=0;i<ballNumber;i++){
+      balls[i] = new ball(400,400,Math.random()*20,20)
+    }
+}
+
 function render(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    box.draw();
+    for(i=0;i<ballNumber;i++){
+        balls[i].draw();
+      }
     requestAnimationFrame(render);
 }
 
+init();
 render();
